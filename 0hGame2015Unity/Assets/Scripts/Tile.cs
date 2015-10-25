f32ab814f59fccfd25fb20ae824032599c37b98c
savam <savam@nordeus.eu>
2015-10-25 02:44:13 +0100

    Added tiles, basic textures and clicking

---
+++ 0hGame2015Unity/Assets/Scripts/Tile.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Tile : MonoBehaviour
+{
+
+	public enum TileType
+	{
+		Green = 0,
+		Red,
+		Blue,
+		Yellow,
+		Orange
+	};
+
+	public bool IsVisible;
+	public TileType Type = TileType.Yellow;
+
+	private Renderer renderer;
+	private TileContainer container;
+
+	protected void Start()
+	{
+		renderer = GetComponent<Renderer>();
+		container = FindObjectOfType<TileContainer>();
+	}
+
+	protected void Update()
+	{
+		Color color;
+		if (IsVisible)
+		{
+			switch (Type)
+			{
+				case TileType.Green:
+					color = Color.yellow;
+					break;
+				case TileType.Red:
+					color = Color.yellow;
+					break;
+				case TileType.Blue:
+					color = Color.yellow;
+					break;
+				case TileType.Yellow:
+					color = Color.yellow;
+					break;
+				case TileType.Orange:
+					color = Color.yellow;
+					break;
+				default:
+					color = Color.white;
+					break;
+			}
+			renderer.material.mainTexture = container.textures[(int)Type];
+		}
+		else
+		{
+			color = Color.black;
+			renderer.material.mainTexture = null;
+		}
+		renderer.material.color = color;
+	}
+
+}
---
+++ 0hGame2015Unity/Assets/Scripts/TileContainer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TileContainer : MonoBehaviour
+{
+
+	public LayerMask clickableLayers;
+
+	public Texture[] textures;
+
+	private Tile[] tiles;
+
+	public Tile GetTile(int x, int y)
+	{
+		x = Mathf.Clamp(x, 0, 3);
+		y = Mathf.Clamp(y, 0, 3);
+		return tiles[x*4 + y];
+	}
+
+	protected void Start()
+	{
+		tiles = GetComponentsInChildren<Tile>();
+		for (int i = 0; i < tiles.Length; i++)
+		{
+			tiles[i].IsVisible = false;
+		}
+	}
+
+	protected void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			RaycastHit hitInfo;
+			var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray, out hitInfo, Mathf.Infinity, clickableLayers))
+			{
+				Debug.Log("CLICK");
+				var tile = hitInfo.collider.GetComponent<Tile>();
+				if (tile != null)
+				{
+					Debug.Log("TILE");
+					tile.IsVisible = !tile.IsVisible;
+				}
+			}
+		}
+	}
+}
